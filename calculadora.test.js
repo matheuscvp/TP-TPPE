4f58a1503658a0ad3ffb7105b4cd6221d5bca789
lcsAndrade <lucascandradex@protonmail.com>
2022-12-21 19:27:19 -0300

    Falsificação: Cadastro de deduções gerais - VERMELHO

--- calculadora.test.js
+++ calculadora.test.js
@@ -10,3 +10,19 @@ test('Cadastro de rendimento', () => {
     calculadora.cadastrarRendimento("Salario", 5000);
     expect(calculadora.obterTotalRencimentos()).toBe(5000);
 })
+
+
+describe('Cadastro de Deduções', () => {
+  beforeEach(() => {
+    calculadora = new CalculadoraIRPF();
+  });
+
+  it.each([
+    [["Salario", 5000], 5000],
+    [["Salario", 7500], 7500],
+    [["Salario", 11000], 11000],
+  ])("Teste parametrizado de cadastro de rendimentos", (inputValue, result) => {
+    calculadora.cadastrarRendimento(inputValue[0], inputValue[1]);
+    expect(calculadora.obterTotalRendimentos()).toBe(result);
+  });
+});
