a5470742e38ab7733efef4a6fea25d8d766b592c
matheuscvp <matheuscvp12@gmail.com>
2022-12-21 13:22:10 -0300

    Falsificacao - Vermelho

--- calculadora.test.js
+++ calculadora.test.js
@@ -5,3 +5,8 @@ let calculadora;
 beforeEach(() => {
     calculadora = new CalculadoraIRPF();
 });
+
+test('Cadastro de rendimento', () => {
+    calculadora.cadastrarRendimento("Salario", 5000);
+    expect(calculadora.obterTotalRencimentos()).toBe(5000);
+})
