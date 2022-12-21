ab171b2d7a17526429eb5a28fdb473ca171d314b
lcsAndrade <lucascandradex@protonmail.com>
2022-12-21 20:14:48 -0300

    Duplicação: Cadastro de dedução referente a previdencia - VERDE

--- calculadora.js
+++ calculadora.js
@@ -30,6 +30,9 @@ class CalculadoraIRPF {
   }
 
   obterTotalDeducoes() {
+    if (this.previdencia.length === 2) {
+      return this.deducoes.geral + (this.previdencia[0].valor + this.previdencia[1].valor);
+    }
     return this.deducoes.geral + this.deducoes.previdencia;
   }
 
