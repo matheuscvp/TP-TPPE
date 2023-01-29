fe60673062df8f00ba3f906f994b198c54eb7da0
DaviMarinho <davii_marinho@hotmail.com>
2023-01-29 14:10:17 -0300

    Refactoring: extrair método (calculoImposto) Mau-cheiro identificado: código duplicado, método longo Descrição da refatoração: aplicou-se a refatoração para extrair código duplicado no método e para dividir o método original em 5 novos métodos, cada método fazendo um cálculo para a faixa do imposto, cada um com maior coesão, também extraindo as constantes e deixando o código mais claro. Classes/métodos/atributos afetados: * Método calculoImposto foi dividido nos métodos obterPrimeiraFaixa, obterSegundaFaixa, obterTerceiraFaixa, obterQuartaFaixa e obterQuintaFaixa.
    Signed-off-by: DaviMarinho <davii_marinho@hotmail.com>

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,13 @@
+const MAX_FAIXA1 =  1903.98;
+const ALIQUOTA_FAIXA2 = 0.075;
+const MAX_FAIXA2 =  922.67;
+const ALIQUOTA_FAIXA3 = 0.15;
+const MAX_FAIXA3 =  924.40;
+const ALIQUOTA_FAIXA4 = 0.225;
+const MAX_FAIXA4 =  913.63;
+const ALIQUOTA_FAIXA5 = 0.275;
+const VALOR_DEPENDENTE = 189.59;
+
 class CalculadoraIRPF {
   constructor() {
     this.totalRendimento = 0;
@@ -58,26 +68,80 @@ class CalculadoraIRPF {
   }
 
   calculoImposto() {
-    let baseImposto = this.obterBaseCalculo()
-    baseImposto = baseImposto - 1903.98 
-    if (baseImposto < 0){
-      return this.impostoRendimento = 0;
-    } else if (baseImposto <= 922.67) {
-      return this.impostoRendimento = baseImposto * 0.075;
+    let baseImposto = this.obterBaseCalculo();
 
-    } else if (baseImposto > 922.67 && baseImposto <= 1847.07) {
-      baseImposto = baseImposto - 922,67;
-      return this.impostoRendimento = (baseImposto * 0.15) + 69.20;
+    baseImposto = this.obterPrimeiraFaixa(baseImposto);
+    baseImposto = this.obterSegundoFaixa(baseImposto);
+    baseImposto = this.obterTerceiraFaixa(baseImposto);
+    baseImposto = this.obterQuartaFaixa(baseImposto);
+    baseImposto = this.obterQuintaFaixa(baseImposto);
 
-    } else if (baseImposto > 1847.07 && baseImposto <= 2760.7) {
-      baseImposto = baseImposto - 1847.07;
-      return this.impostoRendimento = (baseImposto * 0.225) + 138.66 + 69.20;
-
-    } else if (baseImposto > 2760.7) {
-      baseImposto = baseImposto - 2760.7;
-      return this.impostoRendimento = (baseImposto * 0.275) + 205.56 + 138.66 + 69.20;
-    }
+    return this.impostoRendimento;
   } 
+
+  obterPrimeiraFaixa(baseImposto){
+    let basePrimeiraFaixa = baseImposto;
+
+    if (basePrimeiraFaixa < 0){
+      this.impostoRendimento = 0;
+    } 
+
+    basePrimeiraFaixa = baseImposto - MAX_FAIXA1;
+
+    return basePrimeiraFaixa;
+  }
+
+  obterSegundoFaixa(baseImposto){
+    let baseSegundaFaixa = baseImposto;
+
+    if (baseSegundaFaixa > 0 && baseSegundaFaixa < MAX_FAIXA2) {
+      this.impostoRendimento += baseImposto * ALIQUOTA_FAIXA2;
+      baseSegundaFaixa = 0;
+    } else if (baseSegundaFaixa > MAX_FAIXA2) {
+      this.impostoRendimento += MAX_FAIXA2 * ALIQUOTA_FAIXA2;
+      baseSegundaFaixa = baseImposto - MAX_FAIXA2;
+    }
+
+    return baseSegundaFaixa;
+  }
+
+  obterTerceiraFaixa(baseImposto){
+    let baseTerceiraFaixa = baseImposto;
+
+    if (baseTerceiraFaixa > 0 && baseTerceiraFaixa < MAX_FAIXA3) {
+      this.impostoRendimento += baseImposto * ALIQUOTA_FAIXA3;
+      baseTerceiraFaixa = 0;
+    } else if (baseTerceiraFaixa > MAX_FAIXA3) {
+      this.impostoRendimento += MAX_FAIXA3 * ALIQUOTA_FAIXA3;
+      baseTerceiraFaixa = baseImposto - MAX_FAIXA3;
+    }
+
+    return baseTerceiraFaixa;
+  }
+
+  obterQuartaFaixa(baseImposto){
+    let baseQuartaFaixa = baseImposto;
+
+    if (baseQuartaFaixa > 0 && baseQuartaFaixa < MAX_FAIXA4) {
+      this.impostoRendimento += baseImposto * ALIQUOTA_FAIXA4;
+      baseQuartaFaixa = 0;
+    } else if (baseQuartaFaixa > MAX_FAIXA4) {
+      this.impostoRendimento += MAX_FAIXA4 * ALIQUOTA_FAIXA4;
+      baseQuartaFaixa = baseImposto - MAX_FAIXA4;
+    }
+
+    return baseQuartaFaixa;
+  }
+
+  obterQuintaFaixa(baseImposto){
+    let baseQuintaFaixa = baseImposto;
+
+    if (baseQuintaFaixa > 0) {
+      this.impostoRendimento += baseImposto * ALIQUOTA_FAIXA5;
+    }
+
+    return baseQuintaFaixa;
+  }
 
   obterAliquota(){
     this.calculoImposto();  
@@ -85,10 +149,10 @@ class CalculadoraIRPF {
 
     return this.aliquota = impost*100/this.totalRendimento;
   }
-
+  
 
   obterTotalDeducoes() {
-    return this.deducoes.geral + this.deducoes.previdencia + this.deducoes.pensao + (189.59 * this.dependentes.length);
+    return this.deducoes.geral + this.deducoes.previdencia + this.deducoes.pensao + (VALOR_DEPENDENTE * this.dependentes.length);
   }
 
   obterListaDeducoesGerais() {
